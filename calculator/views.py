4f8412df2ae7a47ac8eb64ff71f1536a60668090
sujallohani10 <sujal.lohani7@gmail.com>
2021-02-11 23:12:05 +1100

    some categories items functionality added

--- calculator/views.py
+++ calculator/views.py
@@ -13,12 +13,11 @@ def index(request):
 def carrotAndCelery(request):
     return render(request, 'calculator.html', {'urlName':'calculateCarrrotCelery','badge_name':'Carrot & Celery 350G (2080)'})
 
-
 def carrotBattons(request):
 
     url = 'calculateCommonMethod'
     badge_name = 'Carrot Battons 200G (2092)'
-    weight = 200
+    weight = 205
     displayMessage = "Carrot weight in Kg: "
     template_view =  'common.html'
 
@@ -28,7 +27,7 @@ def carrotCurls(request):
 
     url = 'calculateCommonMethod'
     badge_name = 'Carrot Curls 400G (9566)'
-    weight = 400
+    weight = 405
     displayMessage = "Carrot weight in Kg: "
     template_view =  'common.html'
 
@@ -38,12 +37,61 @@ def dicedPumpkin(request):
 
     url = 'calculateCommonMethod'
     badge_name = 'Diced Pumpkin 500G (2046)'
-    weight = 500
+    weight = 505
     displayMessage = "Pumpkin weight in Kg: "
     template_view =  'common.html'
 
     return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
 
+def zucchini(request):
+
+    url = 'calculateCommonMethod'
+    badge_name = 'Zucchini Zoodles 400G (8968)'
+    weight = 405
+    displayMessage = "Zucchini weight in Kg: "
+    template_view =  'common.html'
+
+    return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
+
+def beetroot(request):
+
+    url = 'calculateCommonMethod'
+    badge_name = 'Beetroot Noodles 400G (4010)'
+    weight = 405
+    displayMessage = "Beetroot weight in Kg: "
+    template_view =  'common.html'
+
+    return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
+
+def cauliflowerRice(request):
+
+    url = 'calculateCommonMethod'
+    badge_name = 'Cauliflower Rice 350G (7095)'
+    weight = 355
+    displayMessage = "Cauliflower weight in Kg: "
+    template_view =  'common.html'
+
+    return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
+
+def shreddedKale(request):
+
+    url = 'calculateCommonMethod'
+    badge_name = 'Shredded Kale 150G (2098)'
+    weight = 155
+    displayMessage = "Kale weight in Kg: "
+    template_view =  'common.html'
+
+    return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
+
+def mixCapsicum(request):
+
+    url = 'calculateCommonMethod'
+    badge_name = 'Mix Capsicum 300G (2083)'
+    weight = 100
+    displayMessage = "Green, Yellow & Red Capsicum each weight in Kg: "
+    template_view =  'common.html'
+
+    return render(request, template_view, {'urlName':url, 'badge_name':badge_name, 'weight': weight, 'message':displayMessage})
 
 def calculateCarrrotCelery(request):
     # validating...i.e. request should be ajax and method should be POST.
